a9cc628387036ca39e5946ebe66da5078ec5379c
Daniel <dmorell@comillas.edu>
2024-11-04 18:47:38 +0100

    Mision 5 Done

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -7,23 +7,27 @@ public class EnemyMover : MonoBehaviour
     [SerializeField]
     List<Waypoint> path = new List<Waypoint>();
 
+    [SerializeField] 
+    float waitTime = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(var waypoint in path)
         {
-            Debug.Log(waypoint.transform.name);
+            transform.localPosition = waypoint.transform.localPosition;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
