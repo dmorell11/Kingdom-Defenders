a1bd36132a2930555f2ce21f1b538793a6b10270
Daniel <dmorell@comillas.edu>
2024-11-20 13:10:08 +0100

    Mision 15 Done

--- Assets/Ballista/TargetLocator.cs
+++ Assets/Ballista/TargetLocator.cs
@@ -4,24 +4,55 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
-    [SerializeField]
-    Transform weapon;
-
+    [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
+        if (target == null)
+        {
+            Attack(false);
+            return;
+        }
+
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+
         weapon.LookAt(target);
+
+        Attack(targetDistance < range);
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
